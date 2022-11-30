44787af976c1ff70523c50e1bc93c3020b1657d7
Dilan_Lappiz <dilanjimenez140@gmail.com>
2022-11-29 22:01:22 -0500

    Emails sent to the providers

--- Src/Entities/Producto/updateStatus.js
+++ Src/Entities/Producto/updateStatus.js
@@ -1,28 +1,24 @@
 setTimeout(() => {
   var status = e.dataItem.EstadoCotizacion;
   var email;
-  var subject = 'Estado de la Cotización';
-  var text = 'Estado de Cotización en revisión';
-  var HTML;
-  var attachments = [
-    {
-      filename: 'test.txt',
-      content: 'Hola mundo Lappiz desde un archivo!'
-    }
-  ]
+  var subject = '';
+  var comments = ''
   var cc = [""]
   var bcc = [""]
-  var now = new Date().toLocaleString().replace(",","").replace(/:.. /," ");
+  var now = new Date().toLocaleString().replace(",", "").replace(/:.. /, " ");
   debugger
   //Proveedor
   if (sessionStorage.rolesId == '12ef9a54-036d-4942-a391-2c9fb6538753') {
     email = JSON.parse(sessionStorage.LappizUser).Email
     cc.push('somuguyibru-6731@yopmail.com')
+    getProveedor(0);
     if (typeof e.dataItem.EstadoCotizacion === 'undefined') {
       console.log('Agregando estado...');
-      getProveedor(0);
-      status = 'revision'
+      status = 'Recibida'
+      subject = `Producto ${e.dataItem.Nombre} recibido`
+      comments = `Hemos recibido su producto ${e.dataItem.Nombre}. Le notificaremos por este mismo medio el momento en que iniciemos el proceso de revisión. Queda tenga un buen día`
       var providerQuery = `UPDATE MercaFruta_Lappiz_Productos SET ProveedorFk = '${sessionStorage.ProveedorFk}', EstadoCotizacion = '${status}', OData = 'A' WHERE Id='${e.dataItem.Id}'`;
+      debugger
       execQuery(providerQuery).then(function (response) {
         var dataResult = response[0];
         //imprimir resultado de la consulta
@@ -32,18 +28,12 @@ setTimeout(() => {
         console.log(error);
       });
 
-      var HTML = `<h1>Estado de Cotización en revisión</h1>`;
-      var attachments = [
-        {
-          filename: 'test.txt',
-          content: 'Hola mundo Lappiz desde un archivo!'
-        }
-      ]
-
-
-    } else if(e.dataItem.EstadoCotizacion == 'Devuelta con Observación'){
-      console.log('Pendiente para el estado faltante')
-      var ODataQuery = `UPDATE MercaFruta_Lappiz_Productos SET OData = 'F', EstadoCotizacion = 'Recibida con Ajuste' WHERE Id='${e.dataItem.Id}'`;
+    } else if (e.dataItem.EstadoCotizacion == 'Devuelta con Observación') {
+      //console.log('Pendiente para el estado faltante')
+      status = 'Recibida con Ajuste'
+      subject = `Producto ${e.dataItem.Nombre} recibido con ajuste`
+      comments = `Hemos recibido su producto ${e.dataItem.Nombre} con los respectivos ajustes solicitados. Le notificaremos en los próximos días si se aprueba o no su cotización. Queda tenga un buen día`
+      var ODataQuery = `UPDATE MercaFruta_Lappiz_Productos SET OData = 'F', EstadoCotizacion = '${status}' WHERE Id='${e.dataItem.Id}'`;
       execQuery(ODataQuery).then(function (response) {
         var dataResult = response[0];
         //imprimir resultado de la consulta
@@ -53,37 +43,34 @@ setTimeout(() => {
         console.log(error);
       });
     }
-
-
-
-
-
-
-
   } else {
     //Administrador de Cotizaciones
+    debugger
+    status = e.dataItem.EstadoCotizacion;
     getProveedor(1)
-
+    let productStatus;
     email = sessionStorage.ProveedorEmail;
-    var HTML = `<h1>Estado de Cotización ${e.dataItem.EstadoCotizacion}
-    </h1>`;
-    var attachments = [
-      {
-        filename: 'test.txt',
-        content: 'Hola mundo Lappiz desde un archivo!'
-      }
-    ]
+    switch (e.dataItem.EstadoCotizacion) {
+      case 'Aprobada':
+        productStatus = 'Aprobado'
+        break;
+
+        case 'No Aprobada':
+        productStatus = 'No Aprobado'
+        break;
+    
+      default: productStatus = 'Devuelto con Observación'
+        break;
+    }
+
+    subject = `Producto ${e.dataItem.Nombre} ${productStatus}`
+    comments = e.dataItem.ObservacionesCotizacion
     cc.push(JSON.parse(sessionStorage.LappizUser).Email)
 
 
   }
-
-
-  sendEmail(email, subject, text, HTML, attachments, cc, bcc).then(function (response) {
-    toastr.info(`Estado de cotización:${status}`);
-  }, function (error) {
-    toastr.warning('Ha ocurrido un error');
-  });
+  //Envío al correo
+  sendMessage(email, subject, cc, bcc, e.dataItem.Nombre, comments, status, sessionStorage.FullName);
 
   function getProveedor(option) {
     debugger
@@ -97,6 +84,8 @@ setTimeout(() => {
         console.log(`Id Proveedor del dataResult: ${dataResult[0].Id}`);
         sessionStorage.ProveedorFk = dataResult[0].Id;
         console.log(`Id Proveedor en el sessionStorage: ${sessionStorage.ProveedorFk}`);
+        sessionStorage.FullName = `${dataResult[0].Nombre} ${dataResult[0].Apellido}`
+        console.log(sessionStorage.FullName);
       }, function (error) {
         console.log(error);
       });
@@ -114,7 +103,6 @@ setTimeout(() => {
 
       var productsQuery = `SELECT ProveedorFk FROM MercaFruta_Lappiz_Productos WHERE Id = '${e.dataItem.Id}'`;
 
-
       execQuery(productsQuery).then(function (response) {
         var dataResult1 = response[0];
         //imprimir resultado de la consulta
@@ -123,18 +111,23 @@ setTimeout(() => {
         console.log(`Id Proveedor del dataResult: ${dataResult1[0].ProveedorFk}`);
         sessionStorage.ProveedorFk = dataResult[0].Id;
         console.log(`Id Proveedor en el sessionStorage: ${sessionStorage.ProveedorFk}`);
-
+      }, function (error) {
+        console.log(error);
       });
-      var emailQuery = `SELECT Email FROM MercaFruta_Lappiz_Proveedor WHERE Id = '${sessionStorage.ProveedorFk}'`
-      execQuery(emailQuery).then(function (response1) {
-        debugger
-        var dataResult2 = response1[0]
-        console.log(dataResult2);
-        console.log(`Email Proveedor del dataResult: ${dataResult2[0].Email}`);
-        sessionStorage.ProveedorEmail = dataResult2[0].Email;
-        console.log(`Email Proveedor en el sessionStorage: ${sessionStorage.ProveedorEmail}`);
 
-      })
+      var emailQuery = `SELECT * FROM MercaFruta_Lappiz_Proveedor WHERE Id = '${sessionStorage.ProveedorFk}'`
+      execQuery(emailQuery).then(function (response1) {
+        var dataResult2 = response[0];
+        //imprimir resultado de la consulta
+        debugger
+        console.log(dataResult2);
+        sessionStorage.FullName = `${dataResult2[0].Nombre} ${dataResult2[0].Apellido}`
+        console.log(sessionStorage.FullName);
+        sessionStorage.ProveedorEmail = dataResult2[0].Email
+        console.log(sessionStorage.ProveedorEmail);
+      }, function (error) {
+        console.log(error);
+      });
     }
 
   }
