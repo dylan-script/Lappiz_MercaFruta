7fbff7edeb254cf04ec0f1701cff6d62b8c3a113
Dilan_Lappiz <dilanjimenez140@gmail.com>
2023-01-11 10:30:33 -0500

    Update Quotations filters

---
+++ Src/Entities/Producto/Resources/SelectPhoto.sql
@@ -0,0 +1,2 @@
+SELECT * FROM FrutaNet_Lappiz_Productos WHERE Id = '39c4933b-a44d-4483-8773-7e74856b83e3'
+[833e07be-83d8-4114-a34f-c9bce0264b74.jpg]
---
+++ Src/Entities/Producto/Resources/SelectProduct.sql
@@ -0,0 +1,7 @@
+SELECT TOP (1000) [Id]
+      ,[OData]
+      ,[EstadoCotizacion]
+      ,[NumeroCotizacion]
+      ,[CodigoCot]
+  FROM [Lappiz_Basic_Apps].[dbo].[FrutaNet_Lappiz_Productos]
+  WHERE Id = 'BFB85B78-AFE5-436D-B2FB-146FB50A96BB'
